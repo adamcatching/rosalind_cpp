faf7e69d53b1da75d2973eb78fae17314130d5f8
adamcatching <adamcatching@gmail.com>
2025-11-03 11:02:56 -0500

    Add comments

--- code/rosalind_01_DNA.cpp
+++ code/rosalind_01_DNA.cpp
@@ -17,18 +17,24 @@ int main() {
     // Define the string to hold DNA string
     string s;
 
+    // Prompt for DNA input, store in s
     cout << "DNA string input:" << endl;
     cin >> s;
 
-    chrono::steady_clock sc;   // create an object of `steady_clock` class
-    auto start = sc.now();     // start timer
+    // Create an object of `steady_clock` class and start timer
+    chrono::steady_clock sc;   
+    auto start = sc.now();
     
+    // Loop through (iterative CPU command, GPU cuda soon)
     for (int i = 0; i < s.length(); i++) {
+        // Print iterator and that position in string
         cout << i << ' ' << s[i] << endl;
     }
 
-    auto end = sc.now();       // end timer (starting & ending is done by measuring the time at the moment the process started & ended respectively)
-    auto time_span = static_cast<chrono::duration<double>>(end - start);   // measure time span between start & end
+    // End timer and print time
+    auto end = sc.now();      
+    auto time_span = static_cast<chrono::duration<double>>(end - start);   
     cout<<"Operation took: "<<time_span.count()<<" seconds !!!";
+    
     return 0;
 }
